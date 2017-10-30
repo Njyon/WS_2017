51ae4c8d1024959f99eb87ce2aa71c13b555dc34
Marek <thenjyon@googlemail.com>
2017-10-30 22:20:51 +0100

    Im tired

--- MyProject/Source/MyProject/MyProjectCharacter.cpp
+++ MyProject/Source/MyProject/MyProjectCharacter.cpp
@@ -8,6 +8,12 @@
 #include "Animation/AnimInstance.h"
 #include "Kismet/GameplayStatics.h"
 #include "MyProject/MyProjectProjectile.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "TimerManager.h"
+#include "PhysicsEngine/BodyInstance.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "ConstructorHelpers.h"
+#include "MyProject/MyProjectProjectile.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogFPChar, Warning, All);
 
@@ -52,7 +58,15 @@ AMyProjectCharacter::AMyProjectCharacter()
 
 	// Default offset from the character location for projectiles to spawn
 	GunOffset = FVector(100.0f, 0.0f, 10.0f);
+
+	movementComponent = GetCharacterMovement();
+	world = GetWorld();
+
 }
+
+			//////////////////////////////////////
+			//////////	  Begin Play	//////////
+			//////////////////////////////////////
 
 void AMyProjectCharacter::BeginPlay()
 {
@@ -63,36 +77,106 @@ void AMyProjectCharacter::BeginPlay()
 
 	FP_Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
 
+	this->movementComponent->SetPlaneConstraintEnabled(true);
 }
 
-//////////////////////////////////////////////////////////////////////////
-// Input
+			//////////////////////////////////////
+			//////////	Input Bind		//////////
+			//////////////////////////////////////
 
-void AMyProjectCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+void AMyProjectCharacter::SetupPlayerInputComponent(class UInputComponent* playerInputComponent)
 {
 	// set up gameplay key bindings
-	check(PlayerInputComponent);
+	check(playerInputComponent);
 
-	//PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
-	//PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+	//playerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	//playerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
-	//PlayerInputComponent->BindAxis("MoveForward", this, &AMyProjectCharacter::MoveForward);
-	//PlayerInputComponent->BindAxis("MoveRight", this, &AMyProjectCharacter::MoveRight);
+	playerInputComponent->BindAxis("MoveForward", this, &AMyProjectCharacter::MoveForward);
+	playerInputComponent->BindAxis("MoveRight", this, &AMyProjectCharacter::MoveRight);
 
-	PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &AMyProjectCharacter::Shoot);
+	/// Input Left Mouse Button
+	playerInputComponent->BindAction("Fire", IE_Pressed, this, &AMyProjectCharacter::LMBPressed);
+	playerInputComponent->BindAction("Fire", IE_Released, this, &AMyProjectCharacter::LMBReleased);
+	/// Input Right Mouse Button
+	playerInputComponent->BindAction("Slomo", IE_Pressed, this, &AMyProjectCharacter::RMBPressed);
+	playerInputComponent->BindAction("Slomo", IE_Released, this, &AMyProjectCharacter::RMBReleased);
 
 	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
 	// "turn" handles devices that provide an absolute delta, such as a mouse.
 	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
 
-	//PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-	//PlayerInputComponent->BindAxis("TurnRate", this, &AMyProjectCharacter::TurnAtRate);
-	//PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
-	//PlayerInputComponent->BindAxis("LookUpRate", this, &AMyProjectCharacter::LookUpAtRate);
+	//playerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	//playerInputComponent->BindAxis("TurnRate", this, &AMyProjectCharacter::TurnAtRate);
+	//playerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+	//playerInputComponent->BindAxis("LookUpRate", this, &AMyProjectCharacter::LookUpAtRate);
 }
 
-void AMyProjectCharacter::Shoot()
+			//////////////////////////////////////
+			//////////		TICK		//////////
+			//////////////////////////////////////
+
+void AMyProjectCharacter::Tick(float deltaTime)
 {
+	Super::Tick(deltaTime);
+
+	///Shoot
+	if (isLMBPressed == true)
+	{
+		if (this->isBulletFired == false)
+		{
+			this->isBulletFired = true;
+			SpawnBullet();		// Shoot a Bullet
+		}
+		else
+		{
+			///InCrease Shoot speed when Slomo is Active
+			if (isSlomoActive)
+			{
+				if (world->GetTimerManager().IsTimerActive(timeHandle) == true && isShootingInNormalSpeed == true)
+				{
+					isShootingInNormalSpeed = false;
+					world->GetTimerManager().ClearTimer(timeHandle); // Cleartimer if u switch into Slomo mode
+				}
+				else if (world->GetTimerManager().IsTimerActive(timeHandle) == false) // Trigger Only Once and than wait for Cooldown (Cooldwon is Rewriteble)
+				{
+					world->GetTimerManager().SetTimer(timeHandle, this, &AMyProjectCharacter::BulletCooldown, fireRateSlomo, false); // Shoot Cooldown
+				}
+			}
+			///Shoot Speed = Normal
+			else
+			{
+				if (world->GetTimerManager().IsTimerActive(timeHandle) == false) // Trigger Only Once and than wait for Cooldown (Cooldwon is Rewriteble)
+				{
+					world->GetTimerManager().SetTimer(timeHandle, this, &AMyProjectCharacter::BulletCooldown, firerateNoSlomo, false); // Shoot Cooldown
+
+					if (isShootingInNormalSpeed == false)
+					{
+						isShootingInNormalSpeed = true;
+					}
+				}
+			}
+		}
+	}
+
+	///Slomo
+	if (isSlomoActive == true)
+	{
+		if (movementComponent->IsFalling() == false /* and / or Sliding  */ )  // check if Character is in "Action"
+		{
+			isSlomoActive = false;
+			UGameplayStatics::SetGlobalTimeDilation(world, 1); // Set Time Dilation to Normal
+		}
+	}
+}
+
+			//////////////////////////////////////
+			//////////	    Input       //////////
+			//////////////////////////////////////
+
+void AMyProjectCharacter::LMBPressed()
+{
+	isLMBPressed = true;
 	// try and fire a projectile
 	if (ProjectileClass != NULL)
 	{
@@ -129,33 +213,109 @@ void AMyProjectCharacter::Shoot()
 	}
 }
 
-void AMyProjectCharacter::MoveForward(float Value)
+void AMyProjectCharacter::LMBReleased()
 {
-	if (Value != 0.0f)
+	isLMBPressed = false;
+}
+
+void AMyProjectCharacter::RMBPressed()
+{
+	if (movementComponent->IsFalling() == true /* and / or Slinding */ ) // Check if you can Set Slomo to Active
 	{
-		// add movement in that direction
-		AddMovementInput(GetActorForwardVector(), Value);
+		isSlomoActive = true;
+		
+		UGameplayStatics::SetGlobalTimeDilation(world, slomoTimeDilation); // Set Time to Slomo Time Dilation
 	}
 }
 
-void AMyProjectCharacter::MoveRight(float Value)
+void AMyProjectCharacter::RMBReleased()
 {
-	if (Value != 0.0f)
+	isSlomoActive = false;
+	UGameplayStatics::SetGlobalTimeDilation(world, 1); // Set Time to Noraml
+}
+
+				//////////////////////////////////////
+				//////////	  Functions     //////////
+				//////////////////////////////////////
+
+void AMyProjectCharacter::MoveForward(float value)
+{
+	if (value != 0.0f)
 	{
 		// add movement in that direction
-		AddMovementInput(GetActorRightVector(), Value);
+		AddMovementInput(GetActorForwardVector(), value);
+		this->VAxis = value;
 	}
 }
 
-void AMyProjectCharacter::TurnAtRate(float Rate)
+void AMyProjectCharacter::MoveRight(float value)
 {
-	// calculate delta for this frame from the rate information
-	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+	if (value != 0.0f)
+	{
+		// add movement in that direction
+		AddMovementInput(GetActorRightVector(), value);
+		this->HAxis = value;
+	}
 }
 
-void AMyProjectCharacter::LookUpAtRate(float Rate)
+void AMyProjectCharacter::TurnAtRate(float rate)
 {
 	// calculate delta for this frame from the rate information
-	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+	AddControllerYawInput(rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
 }
+
+void AMyProjectCharacter::LookUpAtRate(float rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerPitchInput(rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+void AMyProjectCharacter::BulletCooldown()
+{
+	this->isBulletFired = false;
+}
+
+void AMyProjectCharacter::SpawnBullet()
+{
+	////////////////////////////////////////////////
+	// Cast A Ray form the Camera to the Crosshair
+	////////////////////////////////////////////////
+
+	FVector camPos = FirstPersonCameraComponent->GetComponentTransform().GetLocation(); // Get Camera Position (World)
+
+	FVector camForwardVec = FirstPersonCameraComponent->GetComponentTransform().GetRotation().GetForwardVector(); //Get Forward Vector from Camera
+	camForwardVec = camForwardVec * 50000; // Make the Vector longer
+
+	FVector endRaycastPos = camPos + camForwardVec; // Get a EndPosition for the Raycast
+
+	FCollisionQueryParams fireTraceParams = FCollisionQueryParams(FName(TEXT("CamTrace")), true, this);
+	fireTraceParams.bTraceComplex = false;
+	fireTraceParams.bTraceAsyncScene = false;
+	fireTraceParams.bReturnPhysicalMaterial = false;
+	FHitResult hitResult(ForceInit);
+
+
+	world->LineTraceSingleByChannel(	// Raycast
+		hitResult,						// Result
+		camPos,							// RayStart
+		endRaycastPos,					// RayEnd
+		ECC_Pawn, 
+		fireTraceParams);
+
+	if (hitResult.IsValidBlockingHit() == true)
+	{
+		FRotator newRotation = UKismetMathLibrary::FindLookAtRotation(
+			FP_MuzzleLocation->GetComponentTransform().GetLocation(),
+			hitResult.ImpactPoint);
+
+		FActorSpawnParameters spawnInfo;
+		spawnInfo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+		spawnInfo.Owner = this;
+		spawnInfo.Instigator = this;
+
+		//static ConstructorHelpers::FObjectFinder<UBlueprint> projectile(TEXT("Blueprint'/Game/Dynamic/Player/Behaviour/PlayerProjectile'"));
+
+		MyProjectProjectile* projectile = world->SpawnActor<MyProjectProjectile>(playerProjectile, FP_MuzzleLocation->GetComponentTransform().GetLocation(), newRotation, spawnInfo);
+	}
+}
+
 
--- MyProject/Source/MyProject/MyProjectCharacter.h
+++ MyProject/Source/MyProject/MyProjectCharacter.h
@@ -13,6 +13,10 @@ class AMyProjectCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+						//////////////////////////////////////
+						//////////		BODY		//////////
+						//////////////////////////////////////
+
 	/** Pawn mesh: 1st person view (arms; seen only by self) */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Mesh, meta = (AllowPrivateAccess = "true"))
 	class USkeletalMeshComponent* Mesh1P;
@@ -29,28 +33,36 @@ class AMyProjectCharacter : public ACharacter
 	UPROPERTY(EditAnywhere, Category = Mesh)
 		class USceneComponent* FP_MuzzleLocation;
 
-public:
-	AMyProjectCharacter();
+public:								////// PUBLIC //////
+	AMyProjectCharacter();   // Konstructor
 
-protected:
-	virtual void BeginPlay();
+										// UPROPERTY //
 
-public:
+	//FireRate
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = FireRate)
+		float fireRateSlomo = 0.05f;											//Set Fire Rate in Slomo
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = FireRate)
+		float firerateNoSlomo = 0.3f;											//Set Fire Rate
+
+	// is Slomo Active or Deactive
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Slomo)
+		bool isSlomoActive;
+
+	// set Time Dilation
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Slomo)
+		float slomoTimeDilation = 0.05f;
+
 	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
-	float BaseTurnRate;
+		float BaseTurnRate;
 
 	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
-	float BaseLookUpRate;
+		float BaseLookUpRate;
 
 	/** Gun muzzle's offset from the characters location */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay)
-	FVector GunOffset;
-
-	// Fires a projectile.
-	UFUNCTION(BlueprintCallable, Category = "Shoot")
-	void Shoot();
+		FVector GunOffset;
 
 	/** Sound to play each time we fire */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
@@ -67,29 +79,68 @@ public:
 	UPROPERTY(EditDefaultsOnly, Category = Projectile)
 		TSubclassOf<class AMyProjectProjectile> ProjectileClass;
 
-protected:
+										// UFUNCTION //
+
+	/// Input
+	void LMBPressed(); // Left Mouse Button Pressed
+	void LMBReleased(); // Left Mouse Button Released
+	void RMBPressed(); // Right Mouse Button Pressed
+	void RMBReleased(); // Right Mouse Button Released
+
+
+
+	/** Returns Mesh1P subobject **/
+	FORCEINLINE class USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
+	/** Returns FirstPersonCameraComponent subobject **/
+	FORCEINLINE class UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
+
+private:								////// PRIVATE //////
+
+											// UPROPERTY //
+	///DataType
+	float HAxis;	// Horizontal Axis
+	float VAxis;	// Vertical Axis
+	bool isLMBPressed;
+	bool isBulletFired = false;
+	bool isShootingInNormalSpeed; // Check you switched the Time Dilation
+	///Struct
+	FTimerHandle timeHandle;
+	FBodyInstance* camRay; //RayCast from Camera
+	///Class
+	class UCharacterMovementComponent* movementComponent; // Movement Component
+	class UWorld* world;
+	class MyProjectProjectile* projectile;
+	TSubclassOf<MyProjectProjectile> playerProjectile;
+
+											// UFUNCTION //
+
+	void SpawnBullet();
+	void BulletCooldown();
+
+protected:								////// Protected //////
+	virtual void BeginPlay(); //Executes at Begin+
+	virtual void Tick(float deltaTime) override;
 
 	/** Handles moving forward/backward */
-	void MoveForward(float Val);
+	void MoveForward(float val);
 
 	/** Handles stafing movement, left and right */
-	void MoveRight(float Val);
+	void MoveRight(float val);
 
 	/**
 	 * Called via input to turn at a given rate.
 	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
 	 */
-	void TurnAtRate(float Rate);
+	void TurnAtRate(float rate);
 
 	/**
 	 * Called via input to turn look up/down at a given rate.
 	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
 	 */
-	void LookUpAtRate(float Rate);
+	void LookUpAtRate(float rate);
 
-protected:
 	// APawn interface
-	virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
+	virtual void SetupPlayerInputComponent(UInputComponent* inputComponent) override;
 	// End of APawn interface
 
 	/* 
@@ -98,12 +149,5 @@ protected:
 	 * @param	InputComponent	The input component pointer to bind controls to
 	 * @returns true if touch controls were enabled.
 	 */
-
-public:
-	/** Returns Mesh1P subobject **/
-	FORCEINLINE class USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
-	/** Returns FirstPersonCameraComponent subobject **/
-	FORCEINLINE class UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
-
 };
 
