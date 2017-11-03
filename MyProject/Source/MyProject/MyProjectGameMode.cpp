53753e38052e96c32cf9f92e2a1427107ba9323f
RastaPasta <pepperbel@hotmail.com>
2017-11-03 22:04:02 +0100

    NOW

--- MyProject/Source/MyProject/MyProjectGameMode.cpp
+++ MyProject/Source/MyProject/MyProjectGameMode.cpp
@@ -9,7 +9,7 @@ AMyProjectGameMode::AMyProjectGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Blueprint/Player/Behaviour/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Blueprints/Player/Behaviour/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
