ecc4c2ff61b630b437c146c9e4d8f68a49448c9a
PasteteDoeniel <daniel@nolting.de>
2017-11-29 16:06:49 +0100

    sprint sound/ UE logo added/ pause menu

--- MyProject 4.18/Source/MyProject/MyProjectCharacter.h
+++ MyProject 4.18/Source/MyProject/MyProjectCharacter.h
@@ -309,7 +309,6 @@ private:								////// PRIVATE //////
 	bool sliding = false;
 	bool isFalling = false;
 	bool climbingSoundDoOnce = false;
-	//bool WallrunSoundDoOnce = false;
 	bool isShootingLeft = false;
 	bool canSprint = false;
 	
